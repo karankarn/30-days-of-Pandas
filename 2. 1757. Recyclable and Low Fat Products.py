7cb40823c1fa2f937b219c32c6db41f62174ea11
karankarn <114553567+karankarn@users.noreply.github.com>
2025-02-01 16:51:14 +1000

    Add files via upload

---
+++ 2. 1757. Recyclable and Low Fat Products.py
@@ -0,0 +1,32 @@
+import pandas as pd
+data = [['0', 'Y', 'N'], ['1', 'Y', 'Y'], ['2', 'N', 'Y'], ['3', 'Y', 'Y'], ['4', 'N', 'N']]
+products = pd.DataFrame(data, columns=['product_id', 'low_fats', 'recyclable']).astype({'product_id':'int64', 'low_fats':'category', 'recyclable':'category'})
+
+"""Write a solution to find the ids of products that are both low fat and recyclable.
+Return the result table in any order.
+The result format is in the following example.
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Only products 1 and 3 are both low fat and recyclable.
+"""
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products[(products["low_fats"] == "Y") & (products["recyclable"] == "Y")][["product_id"]]
+
+x = find_products(products)
