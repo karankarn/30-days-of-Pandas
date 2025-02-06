ad31e9fe295adfc1238f8d240541a8ada04e2ec1
karankarn <114553567+karankarn@users.noreply.github.com>
2025-02-06 16:17:14 +1000

    Add files via upload

---
+++ 7. 1873. Calculate Special Bonus.py
@@ -0,0 +1,65 @@
+import pandas as pd
+data = [[2, 'Meir', 3000], [3, 'Michael', 3800], [7, 'Addilyn', 7400], [8, 'Juan', 6100], [9, 'Kannon', 7700]]
+employees = pd.DataFrame(data, columns=['employee_id', 'name', 'salary']).astype({'employee_id':'int64', 'name':'object', 'salary':'int64'})
+
+"""+-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key (column with unique values) for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+ 
+
+Write a solution to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee's name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+---------+--------+
+| employee_id | name    | salary |
++-------------+---------+--------+
+| 2           | Meir    | 3000   |
+| 3           | Michael | 3800   |
+| 7           | Addilyn | 7400   |
+| 8           | Juan    | 6100   |
+| 9           | Kannon  | 7700   |
++-------------+---------+--------+
+Output: 
++-------------+-------+
+| employee_id | bonus |
++-------------+-------+
+| 2           | 0     |
+| 3           | 0     |
+| 7           | 7400  |
+| 8           | 0     |
+| 9           | 7700  |
++-------------+-------+
+Explanation: 
+The employees with IDs 2 and 8 get 0 bonus because they have an even employee_id.
+The employee with ID 3 gets 0 bonus because their name starts with 'M'.
+The rest of the employees get a 100% bonus."""
+
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    # subset of employees where employee_id is odd
+    a = employees[employees["employee_id"]%2 != 0]
+    # subset of a that does not contain "M"
+    a = a[~a["name"].str.contains("M")]
+    # create new column bonus for the subset
+    a["bonus"] = a["salary"]*1
+    # merge back to employees using outer join to get union of both frames
+    a = pd.merge(employees, a, how = "outer").fillna(0)
+    # return the dataframe
+    return a.loc[:,["employee_id","bonus"]]
+
+calculate_special_bonus(employees)
